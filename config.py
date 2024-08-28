11a67bee9c46ed912ba2f4de494a831d72ce06c6
SGVp15 <phosphorusp15@mail.ru>
2024-08-28 20:01:29 +0300

    dirs

--- config.py
+++ config.py
@@ -17,9 +17,9 @@ os.makedirs(TEMPLATES_DIR, exist_ok=True)
 
 # ---------- EXCEL --------------
 FILE_XLSX = '//192.168.20.100/Administrative server/РАБОТА АДМИНИСТРАТОРА/ОРГАНИЗАЦИЯ КУРСОВ/Нумерация с 2015 года.xlsx'
-FILE_XLSX = 'C:/Users/user/PycharmProjects/Ite_pdf_creator/Нумерация с 2015 года.xlsx'
+# FILE_XLSX = 'C:/Users/user/PycharmProjects/Ite_pdf_creator/Нумерация с 2015 года.xlsx'
 TEMPLATES_DIR = '//192.168.20.100/Administrative server/РАБОТА АДМИНИСТРАТОРА/ОРГАНИЗАЦИЯ КУРСОВ/ШАБЛОНЫ удостоверений'
-TEMPLATES_DIR = 'C:/Users/user/PycharmProjects/Ite_pdf_creator/data/templates'
+# TEMPLATES_DIR = 'C:/Users/user/PycharmProjects/Ite_pdf_creator/data/templates'
 # FILE_XLSX_TEMP = f'./data/templates/temp.xlsx'
 PAGE_NAME = '2015'
 
