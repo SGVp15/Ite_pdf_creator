0bd0d7ee90c6b8efb0d2acbfc9c9e229b90c7418
SGVp15 <phosphorusp15@mail.ru>
2024-09-02 15:57:48 +0300

    config

--- main.py
+++ main.py
@@ -58,12 +58,12 @@ def create_docx_and_pdf(contacts: [Contact]):
             try:
                 docx2pdf.convert(contact.files_out_docx[file_name], contact.files_out_pdf[file_name])
                 log.info(f'[CREATE_PDF] {contact.sert_number} {contact.files_out_pdf}')
+                if DELETE_DOCX_AFTER_PDF:
+                    path_docx = os.path.dirname(contact.files_out_docx[file_name])
+                    delete_empty_folder(path_docx
             except Exception as e:
                 log.error(f'[CREATE_PDF] {contact.sert_number} {e}')
 
-            if DELETE_DOCX_AFTER_PDF:
-                path_docx = os.path.dirname(contact.files_out_docx[file_name])
-                delete_empty_folder(path_docx)
     print('[ OK ]')
 
     print('Создаю архив ... ', end='')
